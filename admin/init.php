40cb5fd5cdd88d7c5abbb29051ae18ae4a7eaab1
Ahmad <dev.ahmedwael@gmail.com>
2017-10-10 14:41:11 +0200

    building main settings menu

--- admin/init.php
+++ admin/init.php
@@ -5,7 +5,7 @@
 
 add_action( 'admin_menu', 'sv_admin_menu_pages' );
 function sv_admin_menu_pages() {
-	add_menu_page( 'Main setting', 'WP Save Posts', 'manage_options', 'sv_main_setting_page', 'sv_main_setting_page', 'dashicons-tickets', 40);
+	add_menu_page( 'Main setting', 'WP Save Posts', 'manage_options', 'sv_main_setting_page', 'sv_main_setting_page', 'dashicons-tickets', 40 );
 
 
 	add_submenu_page( 'sv_main_setting_page', 'other settings', 'Other Settings', 'manage_options', 'sv_other_setting_page', 'sv_other_setting_page' );
@@ -17,4 +17,18 @@ function sv_main_setting_page() {
 
 function sv_other_setting_page() {
 
+}
+
+add_action( 'admin_init', 'sv_admin_settings' );
+function sv_admin_settings() {
+	register_setting( 'main_sv_options', 'sv_main_settings' );
+}
+
+if ( is_admin() ) {
+	add_action( 'admin_init', 'register_mysettings' );
+}
+
+function register_mysettings() { // whitelist options
+	register_setting( 'main_sv_options', 'new_option_name' );
+	register_setting( 'main_sv_options', 'max_posts_no' );
 }
--- admin/main-setting-template.php
+++ admin/main-setting-template.php
@@ -1,2 +1,22 @@
-Test test
-<?php
+<div class="wrap">
+    <h1>WP Save posts</h1>
+    <p>Save posts to read them later</p>
+    <form method="post" action="options.php">
+		<?php settings_fields( 'main_sv_options' ); ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">New Option Name</th>
+                <td><input type="text" name="new_option_name"
+                           value="<?php echo esc_attr( get_option( 'new_option_name' ) ); ?>"/></td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">Maximum number of saved posts per user</th>
+                <td><input type="number" name="max_posts_no"
+                           value="<?php echo esc_attr( ( get_option( 'max_posts_no' ) ) ? get_option( 'max_posts_no' ) : 8 ); ?>"/>
+                </td>
+            </tr>
+        </table>
+		<?php submit_button(); ?>
+    </form>
+</div>
