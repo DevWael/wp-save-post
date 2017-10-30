5f72a00db790cb60c3b19a52eaddae34c2dd1713
Ahmad Wael <dev.ahmedwael@gmail.com>
2017-10-30 12:39:29 +0200

    building the plugin system...

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -57,30 +57,30 @@ jQuery(document).ready(function ($) {
     });
 
     //Delete post from the list
-    // $(".sv-post-container .sv-post-control button").click(function (e) {
-    //     e.preventDefault();
-    //     post_id = $(this).data("post-id");
-    //     nonce = $(this).data("nonce");
-    //     control = $(this).data("control");
-    //     $.ajax({
-    //         type: "post",
-    //         dataType: "json",
-    //         url: sv_ajax_object.sv_ajax_url,
-    //         data: {
-    //             action: "sv_post_id",
-    //             post_id: post_id,
-    //             nonce: nonce,
-    //             control: control
-    //         },
-    //         success: function (response) {
-    //             console.log(response);
-    //             //remove the post
-    //             $('.sv-post-container.sv-post-' + response.data.post_id).fadeOut(300, function () {
-    //                 $(this).remove();
-    //             });
-    //
-    //             // $('.post-box-meta').append(response.data.button);
-    //         }
-    //     });
-    // });
+    $(".sv-post-container .sv-post-control button").click(function (e) {
+        e.preventDefault();
+        post_id = $(this).data("post-id");
+        nonce = $(this).data("nonce");
+        control = $(this).data("control");
+        $.ajax({
+            type: "post",
+            dataType: "json",
+            url: sv_ajax_object.sv_ajax_url,
+            data: {
+                action: "sv_post_id",
+                post_id: post_id,
+                nonce: nonce,
+                control: control
+            },
+            success: function (response) {
+                console.log(response);
+                //remove the post
+                $('.sv-post-container.sv-post-' + response.data.post_id).fadeOut(300, function () {
+                    $(this).remove();
+                });
+
+                // $('.post-box-meta').append(response.data.button);
+            }
+        });
+    });
 });
